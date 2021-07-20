6aceb6bccefef6a920814582382c656f1c883646
Ai Xin <aixin.0000@bytedance.com>
2021-07-20 18:20:32 +0800

    Refactor: Refactor reorder

--- reorder.go
+++ reorder.go
@@ -7,129 +7,120 @@ import (
 
 // Reorder 接口，目前有不乱序，概率乱序，gap乱序三种模型
 type Reorder interface {
-	// 返回需要增加的延迟时间 (可能为负值)
-	// 通过将该包更"早"发送实现乱序
-	Reorder(rand *rand.Rand) float32
+	// Reorder 返回需要增加的延迟时间 (可能为负值)，通过将该包更"早"发送实现乱序
+	Reorder() time.Duration
 }
 
 // NoneReorder 一律不乱序，不减少延迟时间
 type NoneReorder struct {
-	rand *rand.Rand
 }
 
 var _ Reorder = &NoneReorder{}
 
-func NewNoneReorder(rand *rand.Rand) PacketHandler {
-	reorder := &NoneReorder{rand: rand}
+func NewNoneReorder() PacketHandler {
+	reorder := &NoneReorder{}
 	return reorder.PacketHandler
 }
 
-func (nr *NoneReorder) Reorder(*rand.Rand) float32 {
+func (nr *NoneReorder) Reorder() time.Duration {
 	return 0
 }
 
-func (nr *NoneReorder) PacketHandler(*Packet, *PacketQueue) (delayTime time.Duration, isLoss bool) {
-	return time.Microsecond * time.Duration(nr.Reorder(nr.rand)), false
+func (nr *NoneReorder) PacketHandler(*Packet, *PacketQueue) (time.Duration, bool) {
+	return nr.Reorder(), false
 }
 
 // NormalReorder 概率乱序模型
 // 以 correlation 的概率和上一个包乱序情况相同
-// 剩余情况以 percent 概率乱序
+// 剩余情况以 possibility 概率乱序
 type NormalReorder struct {
-	avgDelay    float32
-	percent     float32
-	correlation float32
+	delay       time.Duration
+	possibility float64
+	correlation float64
 	lastReorder bool
-	rand        *rand.Rand
+	random      *rand.Rand
 }
 
 var _ Reorder = &NormalReorder{}
 
-func NewNormalReorder(avgDelay float32, percent float32,
-	correlation float32, rand *rand.Rand) PacketHandler {
+func NewNormalReorder(delay time.Duration, possibility, correlation float64, random *rand.Rand) PacketHandler {
 	reorder := &NormalReorder{
-		avgDelay:    avgDelay,
-		percent:     percent,
+		delay:       delay,
+		possibility: possibility,
 		correlation: correlation,
 		lastReorder: false, // 默认首包之前的包没有reorder
-		rand:        rand,
+		random:      random,
 	}
 	return reorder.PacketHandler
 }
 
-func (nr *NormalReorder) Reorder(rand *rand.Rand) float32 {
-	if rand.Float32() < nr.correlation {
+func (nr *NormalReorder) Reorder() time.Duration {
+	if nr.random.Float64() < nr.correlation {
 		if nr.lastReorder {
-			return -nr.avgDelay
+			return -nr.delay
 		} else {
 			return 0
 		}
 	}
-	if rand.Float32() >= nr.percent {
-		nr.lastReorder = false
-		return 0
-	} else {
-		nr.lastReorder = true
-		return -nr.avgDelay
+	nr.lastReorder = nr.random.Float64() < nr.possibility
+	if nr.lastReorder {
+		return -nr.delay
 	}
+	return 0
 }
 
-func (nr *NormalReorder) PacketHandler(*Packet, *PacketQueue) (delayTime time.Duration, isLoss bool) {
-	return time.Microsecond * time.Duration(nr.Reorder(nr.rand)), false
+func (nr *NormalReorder) PacketHandler(*Packet, *PacketQueue) (time.Duration, bool) {
+	return nr.Reorder(), false
 }
 
 // GapReorder gap 乱序模型
 // 具体参照tc-netem reorder 功能
 type GapReorder struct {
-	avgDelay    float32
-	percent     float32
-	correlation float32
+	delay       time.Duration
+	possibility float64
+	correlation float64
 	lastReorder bool
 	gap         int
 	count       int // 计数器，近似以gap为周期
-	rand        *rand.Rand
+	random      *rand.Rand
 }
 
 var _ Reorder = &GapReorder{}
 
-func NewGapReorder(avgDelay float32, percent float32,
-	correlation float32, gap int, rand *rand.Rand) PacketHandler {
+func NewGapReorder(delay time.Duration, possibility,
+	correlation float64, gap int, random *rand.Rand) PacketHandler {
 	reorder := &GapReorder{
-		avgDelay:    avgDelay,
-		percent:     percent,
+		delay:       delay,
+		possibility: possibility,
 		correlation: correlation,
 		lastReorder: false, // 默认首包之前的包没有reorder
 		gap:         gap,
 		count:       0,
-		rand:        rand,
+		random:      random,
 	}
 	return reorder.PacketHandler
 }
 
-func (gr *GapReorder) Reorder(rand *rand.Rand) float32 {
-	if rand.Float32() < gr.correlation {
+func (gr *GapReorder) Reorder() time.Duration {
+	if gr.random.Float64() < gr.correlation {
 		if gr.lastReorder {
-			return -gr.avgDelay
-		} else {
-			return 0
+			return -gr.delay
 		}
+		return 0
 	}
-
 	gr.count++
 	if gr.count < gr.gap {
 		gr.lastReorder = false
 		return 0
 	}
-	if rand.Float32() < gr.percent {
+	gr.lastReorder = gr.random.Float64() < gr.possibility
+	if gr.lastReorder {
 		gr.count = 0
-		gr.lastReorder = true
-		return -gr.avgDelay
-	} else {
-		gr.lastReorder = false
-		return 0
+		return -gr.delay
 	}
+	return 0
 }
 
-func (gr *GapReorder) PacketHandler(*Packet, *PacketQueue) (delayTime time.Duration, isLoss bool) {
-	return time.Microsecond * time.Duration(gr.Reorder(gr.rand)), false
+func (gr *GapReorder) PacketHandler(*Packet, *PacketQueue) (time.Duration, bool) {
+	return gr.Reorder(), false
 }
