3ebeff855e23c8a9e2195659c387da087caa3646
Ai Xin <aixin.0000@bytedance.com>
2021-07-23 10:49:00 +0800

    Update: add test for time

--- time_test.go
+++ time_test.go
@@ -1,6 +1,15 @@
 package networksimulator
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestTime(t *testing.T) {
+	testTime := Now()
+	assert.LessOrEqual(t, time.Since(testTime), time.Second)
+}
 
 func BenchmarkTime(b *testing.B) {
 	for i := 0; i < b.N; i++ {
