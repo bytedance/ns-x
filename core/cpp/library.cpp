53f80e8cf9dc3e4060bd1faad2a9c9adb6a10a78
Ricardo Evans <ricardo.a.evans@gmail.com>
2021-07-26 14:37:19 +0800

    Update: make align of time thread safe

--- core/cpp/library.cpp
+++ core/cpp/library.cpp
@@ -1,17 +1,26 @@
 #include "library.h"
 #include <chrono>
+#include <mutex>
 
 using namespace std::chrono;
 
 constexpr auto AlignPeriod = milliseconds(50);
 auto lastSystemTime = system_clock::now();
 auto alignedTime = high_resolution_clock::now();
+std::mutex lock{};
+
+void align(decltype(alignedTime) &tick) {
+    std::lock_guard<std::mutex> lockGuard{lock};
+    if (tick - alignedTime > AlignPeriod) {
+        lastSystemTime = system_clock::now();
+        alignedTime = tick;
+    }
+}
 
 int64_t now() {
     auto tick = high_resolution_clock::now();
     if (tick - alignedTime > AlignPeriod) {
-        lastSystemTime = system_clock::now();
-        alignedTime = tick;
+        align(tick);
     }
     return duration_cast<nanoseconds>(tick - alignedTime + lastSystemTime.time_since_epoch()).count();
 }
