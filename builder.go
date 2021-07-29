8ddc6a48299f9631b94ae45c26890da58db50888
Ai Xin <aixin.0000@bytedance.com>
2021-07-29 12:01:58 +0800

    Update: Group function of nodes

--- builder.go
+++ builder.go
@@ -8,22 +8,29 @@ import (
 
 type Builder interface {
 	Node(node Node) Builder
-	Nodes(nodes ...Node) Builder
+	Group(nodes ...Node) Builder
+	NodeWithName(name string, node Node) Builder
+	GroupWithName(name string, nodes ...Node) Builder
 	NodeByName(name string) Builder
+	GroupByName(name string) Builder
 	Chain() Builder
 	Build() (*Network, map[string]Node)
 }
 
 type builder struct {
-	nodes   map[Node]int
-	names   map[string]Node
-	current Node
+	nodes       map[Node]int
+	names       map[string]Node
+	groups      map[string][]Node
+	current     Node
+	connections map[Node]map[Node]interface{}
 }
 
 func NewBuilder() Builder {
 	return &builder{
-		nodes: map[Node]int{},
-		names: map[string]Node{},
+		nodes:       map[Node]int{},
+		names:       map[string]Node{},
+		groups:      map[string][]Node{},
+		connections: map[Node]map[Node]interface{}{},
 	}
 }
 
@@ -33,20 +40,36 @@ func (b *builder) Chain() Builder {
 }
 
 func (b *builder) Node(node Node) Builder {
+	return b.NodeWithName(node.Name(), node)
+}
+
+func (b *builder) NodeWithName(name string, node Node) Builder {
 	if b.current != nil {
-		b.current.SetNext(append(b.current.GetNext(), node)...)
+		connection, ok := b.connections[b.current]
+		if !ok {
+			connection = map[Node]interface{}{}
+			b.connections[b.current] = connection
+		}
+		connection[node] = nil
 	}
 	if _, ok := b.nodes[node]; !ok {
 		b.nodes[node] = len(b.nodes)
 	}
 	b.current = node
-	if name := node.Name(); name != "" {
+	if name != "" {
 		b.names[name] = node
 	}
 	return b
 }
 
-func (b *builder) Nodes(nodes ...Node) Builder {
+func (b *builder) Group(nodes ...Node) Builder {
+	return b.GroupWithName("", nodes...)
+}
+
+func (b *builder) GroupWithName(name string, nodes ...Node) Builder {
+	if name != "" {
+		b.groups[name] = nodes
+	}
 	builder := Builder(b)
 	for _, node := range nodes {
 		builder = builder.Node(node)
@@ -62,11 +85,22 @@ func (b *builder) NodeByName(name string) Builder {
 	return b.Node(node)
 }
 
+func (b builder) GroupByName(name string) Builder {
+	group, ok := b.groups[name]
+	if !ok {
+		panic("no group with name: " + name)
+	}
+	return b.Group(group...)
+}
+
 func (b *builder) Build() (*Network, map[string]Node) {
 	nodes := make([]Node, len(b.nodes))
 	println("network summary: ")
 	for node, index := range b.nodes {
 		nodes[index] = node
+	}
+	for node, connection := range b.connections {
+		node.SetNext(normalize(connection)...)
 	}
 	for index, node := range nodes {
 		println(b.toString(node, index))
@@ -95,4 +129,12 @@ func (b *builder) toString(node Node, index int) string {
 	}
 	sb.WriteString("]}")
 	return sb.String()
+}
+
+func normalize(nodes map[Node]interface{}) []Node {
+	result := make([]Node, 0, len(nodes))
+	for node := range nodes {
+		result = append(result, node)
+	}
+	return result
 }
