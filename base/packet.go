76ab39024aed5a0175b666175bc76564d1724fa3
Ai Xin <aixin.0000@bytedance.com>
2021-08-09 17:20:24 +0800

    Fix: typo

--- base/packet.go
+++ base/packet.go
@@ -9,8 +9,8 @@ import (
 // SimulatedPacket indicates a simulated packet, with its Actual packet and some simulated environment
 type SimulatedPacket struct {
 	Actual   []byte    // the Actual packet
-	EmitTime time.Time // when this packet is emitted (Where OnEmit a packet means the packet leaves the Where, send to the next Where)
-	SentTime time.Time // when this packet is sent (Where send a packet means the packet enters the Where, waiting to OnEmit)
+	EmitTime time.Time // when this packet is emitted (Where emit a packet means the packet leaves the Where, send to the next Where)
+	SentTime time.Time // when this packet is sent (Where send a packet means the packet enters the Where, waiting to emit)
 	Loss     bool      // whether this packet is lost
 	Where    Node      // where is the packet
 }
