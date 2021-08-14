2f1eaac180b90899a53f239f76516b39f64c7f50
Ai Xin <aixin.0000@bytedance.com>
2021-08-14 15:21:48 +0800

    Update: event chain

--- base/node.go
+++ base/node.go
@@ -5,12 +5,11 @@ import "time"
 // Node Indicates a simulated node in the network
 type Node interface {
 	Name() string
-	Events() *EventBuffer
-	Emit(packet Packet, now time.Time)
+	Transfer(packet Packet, now time.Time) []Event
 	GetNext() []Node
 	SetNext(nodes ...Node)
 	Check()
 }
 
-// OnEmitCallback called when a packet is emitted
-type OnEmitCallback func(packet Packet, target Node, now time.Time)
+// TransferCallback called when a packet is emitted
+type TransferCallback func(packet Packet, target Node, now time.Time)
--- node/basic.go
+++ node/basic.go
@@ -7,19 +7,17 @@ import (
 
 // BasicNode is skeleton implementation of Node
 type BasicNode struct {
-	name           string
-	next           []base.Node
-	events         *base.EventBuffer
-	onEmitCallback base.OnEmitCallback
+	name     string
+	next     []base.Node
+	callback base.TransferCallback
 }
 
 // NewBasicNode creates a new BasicNode
-func NewBasicNode(name string, onEmitCallback base.OnEmitCallback) *BasicNode {
+func NewBasicNode(name string, callback base.TransferCallback) *BasicNode {
 	return &BasicNode{
-		name:           name,
-		next:           []base.Node{},
-		events:         base.NewEventBuffer(),
-		onEmitCallback: onEmitCallback,
+		name:     name,
+		next:     []base.Node{},
+		callback: callback,
 	}
 }
 
@@ -27,19 +25,16 @@ func (n *BasicNode) Name() string {
 	return n.name
 }
 
-func (n *BasicNode) Events() *base.EventBuffer {
-	return n.events
-}
-
-func (n *BasicNode) ActualEmit(packet base.Packet, target base.Node, now time.Time) {
-	target.Emit(packet, now)
-	if n.onEmitCallback != nil {
-		n.onEmitCallback(packet, target, now)
+func (n *BasicNode) ActualEmit(packet base.Packet, target base.Node, now time.Time) []base.Event {
+	if n.callback != nil {
+		n.callback(packet, target, now)
 	}
+	return target.Transfer(packet, now)
 }
 
-func (n *BasicNode) Emit(base.Packet, time.Time) {
+func (n *BasicNode) Transfer(base.Packet, time.Time) []base.Event {
 	panic("not implemented")
+	return nil
 }
 
 func (n *BasicNode) GetNext() []base.Node {
