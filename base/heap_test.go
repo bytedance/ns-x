add1af009b6ebf6b72665cb6c1245a480430b099
Ai Xin <aixin.0000@bytedance.com>
2021-08-18 18:28:33 +0800

    Update: correct test of event heap

--- base/heap_test.go
+++ base/heap_test.go
@@ -10,27 +10,31 @@ import (
 )
 
 func TestHeap(t *testing.T) {
-	packetHeap := &EventHeap{}
+	eventHeap := &EventHeap{}
 	count := 50
 	for i := 0; i < count; i++ {
-		heap.Push(packetHeap, &SimulatedPacket{EmitTime: time.Unix(rand.Int63(), 0)})
+		heap.Push(eventHeap, NewFixedEvent(func(t time.Time) []Event {
+			return nil
+		}, time.Unix(rand.Int63(), 0)))
 	}
-	assert.Equal(t, count, packetHeap.Len())
+	assert.Equal(t, count, eventHeap.Len())
 	last := int64(math.MinInt64)
-	for !packetHeap.IsEmpty() {
-		p := heap.Pop(packetHeap).(*SimulatedPacket)
-		unix := p.EmitTime.Unix()
+	for !eventHeap.IsEmpty() {
+		p := heap.Pop(eventHeap).(Event)
+		unix := p.Time().Unix()
 		assert.GreaterOrEqual(t, unix, last)
 		last = unix
 	}
 }
 
 func BenchmarkHeap(b *testing.B) {
-	packetHeap := &EventHeap{}
+	eventHeap := &EventHeap{}
 	for i := 0; i < b.N; i++ {
-		heap.Push(packetHeap, &SimulatedPacket{EmitTime: time.Unix(rand.Int63(), 0)})
+		heap.Push(eventHeap, NewFixedEvent(func(t time.Time) []Event {
+			return nil
+		}, time.Unix(rand.Int63(), 0)))
 	}
-	for !packetHeap.IsEmpty() {
-		_ = heap.Pop(packetHeap).(*SimulatedPacket)
+	for !eventHeap.IsEmpty() {
+		_ = heap.Pop(eventHeap).(Event)
 	}
 }
