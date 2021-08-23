3ba96fe174e5b4650dc6cf1978d210428e57044b
Ai Xin <aixin.0000@bytedance.com>
2021-08-23 13:25:59 +0800

    Update: add ip packet and udp packet

--- base/packet.go
+++ base/packet.go
@@ -9,3 +9,36 @@ type RawPacket []byte
 func (p RawPacket) Size() int {
 	return len(p)
 }
+
+type IPPacket struct {
+	Version            byte   // 4bit
+	HeaderSize         byte   // 4 bit
+	ServiceType        byte   // 8 bit
+	TotalSize          uint16 // 16 bit
+	Identifier         uint16 // 16 bit
+	Flags              byte   // 3 bit
+	FragmentOffset     uint16 // 13 bit
+	TTL                byte   // 8 bit
+	Protocol           byte   // 8 bit
+	HeaderChecksum     uint16 // 16 bit
+	SourceAddress      uint32 // 32 bit
+	DestinationAddress uint32 // 32 bit
+	Options            []byte // vary
+	Data               Packet
+}
+
+func (p *IPPacket) Size() int {
+	return int(p.TotalSize)
+}
+
+type UDPPacket struct {
+	// Size field has no use
+	SourcePort uint16 // 2 byte
+	TargetPort uint16 // 2 byte
+	Checksum   uint16 // 2 byte
+	Data       Packet
+}
+
+func (p *UDPPacket) Size() int {
+	return p.Data.Size() + 8
+}
