c3cafe5bd9fb3ef1e1c575ade8540b3386ff4b37
Ai Xin <aixin.0000@bytedance.com>
2021-08-26 17:04:40 +0800

    Update: modify queue to suit requirement of bucket sort

--- base/queue_test.go
+++ base/queue_test.go
@@ -2,14 +2,13 @@ package base
 
 import (
 	"github.com/stretchr/testify/assert"
-	"math"
 	"testing"
 )
 
 func TestQueue(t *testing.T) {
-	length := 1000
+	length := 10
 	it := 0
-	ring := NewDataQueue(length)
+	ring := NewQueue(length)
 	data := make([]interface{}, length)
 	for i := 0; i < length; i++ {
 		data[i] = i
@@ -25,51 +24,15 @@ func TestQueue(t *testing.T) {
 func TestQueueOverflow(t *testing.T) {
 	ringLength := 100
 	overflowCycle := 3
-	length := 35
-	it := ringLength*overflowCycle + length - ringLength
-	ring := NewDataQueue(ringLength)
-	data := make([]interface{}, ringLength*overflowCycle+length)
-	for i := 0; i < ringLength*overflowCycle+length; i++ {
+	length := ringLength * overflowCycle
+	ring := NewQueue(ringLength)
+	data := make([]interface{}, length)
+	for i := 0; i < length; i++ {
 		data[i] = i
 		ring.Enqueue(data[i])
 	}
-	assert.Equal(t, ringLength, ring.Length())
-	for ring.Length() > 0 {
-		assert.Equal(t, data[it], ring.Dequeue())
-		it++
-	}
-}
-
-func TestQueueOverflowShouldFail(t *testing.T) {
-	ringLength := 100
-	overflowCycle := 3
-	length := 35
-	it := ringLength*overflowCycle + length - ringLength
-	ring := NewDataQueue(ringLength)
-	data := make([]interface{}, ringLength*overflowCycle+length)
-	for i := 0; i < ringLength*overflowCycle+length; i++ {
-		data[i] = i
-		ring.Enqueue(data[i])
-	}
-	assert.Equal(t, ringLength, ring.Length())
-	for ring.Length() > 0 {
-		assert.Equal(t, data[it], ring.Dequeue())
-		it++
-	}
-}
-
-func TestQueueOverflowWithDequeue(t *testing.T) {
-	ringLength := 100
-	overflowCycle := 1
-	length := 37
-	it := int(math.Max(float64(ringLength*overflowCycle+length-ringLength), float64(length)))
-	ring := NewDataQueue(ringLength)
-	data := make([]interface{}, ringLength*overflowCycle+length)
-	for i := 0; i < ringLength*overflowCycle+length; i++ {
-		data[i] = i
-		ring.Enqueue(data[i])
-	}
-	assert.Equal(t, ringLength, ring.Length())
+	assert.Equal(t, length, ring.Length())
+	it := 0
 	for ring.Length() > 0 {
 		assert.Equal(t, data[it], ring.Dequeue())
 		it++
