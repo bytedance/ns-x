933c04cb9bc45de9511e8ba9f3e6f3b2069adaa2
Ai Xin <aixin.0000@bytedance.com>
2021-08-26 12:00:43 +0800

    Update: update comment for clocks

--- tick/clock.go
+++ tick/clock.go
@@ -2,12 +2,15 @@ package tick
 
 import "time"
 
+// Clock always return current time in its view
 type Clock func() time.Time
 
+// NewRealClock create a clock align to system clock
 func NewRealClock() Clock {
 	return time.Now
 }
 
+// NewStepClock create a clock, return the calculated time and step forward a given duration once called
 func NewStepClock(t time.Time, step time.Duration) Clock {
 	return func() time.Time {
 		result := t
