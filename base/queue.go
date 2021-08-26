63ae3f980636d0e73aec37d0a6a9dc3bbe97a90f
Ai Xin <aixin.0000@bytedance.com>
2021-08-26 17:04:40 +0800

    Update: modify queue to suit requirement of bucket sort

--- base/queue.go
+++ base/queue.go
@@ -1,20 +1,41 @@
 package base
 
-// DataQueue is a ring queue of fixed size, it's usually used as a history record of data
-type DataQueue struct {
+// Queue is a ring queue implemented in array/slice
+// O(1) time complexity for enqueue/dequeue/random access
+// O(n) space complexity, where n is the maximum count of elements in the queue at the same time
+type Queue struct {
 	head, tail, length int
 	storage            []interface{}
 }
 
-func NewDataQueue(length int) *DataQueue {
-	return &DataQueue{head: 0, tail: 0, length: length + 1, storage: make([]interface{}, length+1)}
+func NewQueue(length int) *Queue {
+	storage := make([]interface{}, length)
+	for len(storage) < cap(storage) {
+		storage = append(storage, nil)
+	}
+	return &Queue{head: 0, tail: 0, length: len(storage), storage: storage}
 }
 
-func (q *DataQueue) IsEmpty() bool {
+func (q *Queue) expand() {
+	for i := 0; i < q.tail; i++ {
+		q.storage = append(q.storage, q.storage[i])
+		q.storage[i] = nil
+		q.length++
+	}
+	q.tail = q.length
+	q.storage = append(q.storage, nil)
+	q.length++
+	for q.length < cap(q.storage) {
+		q.storage = append(q.storage, nil)
+		q.length++
+	}
+}
+
+func (q *Queue) IsEmpty() bool {
 	return q.Length() == 0
 }
 
-func (q *DataQueue) Length() int {
+func (q *Queue) Length() int {
 	result := q.tail - q.head
 	for result < 0 {
 		result += q.length
@@ -22,35 +43,33 @@ func (q *DataQueue) Length() int {
 	return result
 }
 
-func (q *DataQueue) Enqueue(data interface{}) {
+func (q *Queue) Enqueue(data interface{}) {
 	q.storage[q.tail] = data
 	q.tail++
 	if q.tail >= q.length {
 		q.tail = 0
 	}
-	if q.head == q.tail {
-		q.head++
-	}
-	if q.head >= q.length {
-		q.head = 0
+	if q.tail == q.head {
+		q.expand()
 	}
 }
 
-func (q *DataQueue) Dequeue() interface{} {
+func (q *Queue) Dequeue() interface{} {
 	if q.head == q.tail {
-		panic("record is empty")
+		panic("queue is empty")
 	}
-	result := q.storage[q.head]
-	q.head++
 	if q.head >= q.length {
 		q.head = 0
 	}
+	result := q.storage[q.head]
+	q.storage[q.head] = nil
+	q.head++
 	return result
 }
 
-func (q *DataQueue) At(index int) interface{} {
+func (q *Queue) At(index int) interface{} {
 	if index >= q.Length() {
-		panic("index is overflow")
+		panic("index overflow")
 	}
 	index += q.head
 	for index >= q.length {
@@ -60,7 +79,7 @@ func (q *DataQueue) At(index int) interface{} {
 }
 
 // Do iterate the queue with the given action
-func (q *DataQueue) Do(action func(interface{})) {
+func (q *Queue) Do(action func(interface{})) {
 	for i := q.head; i != q.tail; i++ {
 		if i >= q.length {
 			i = -1
